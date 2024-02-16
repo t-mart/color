a53c64c005af633524db8c04ec56f909976c1db4
Tim Martin <tim@timmart.in>
2024-02-16 13:50:45 -0600

    Add fade transition on copy tooltip

--- src/lib/Swatch.svelte
+++ src/lib/Swatch.svelte
@@ -21,10 +21,10 @@
 </script>
 
 <button on:click={(e) => copyToClipboard(color, e)} class="flex flex-col items-center relative gap-y-2 hover:underline">
-	<div style="background-color: #{color}" class="h-10 w-10 rounded"></div>
-	<p class="font-mono text-xs">#{color}</p>
+	<div style="background-color: {hashColor}" class="h-10 w-10 rounded"></div>
+	<p class="font-mono text-xs">{hashColor}</p>
     {#if copyTooltipVisible}
-        <div class="absolute bottom-full mb-2">
+        <div class="absolute bottom-full mb-2" transition:fade={{ duration: 200 }}>
             <p class="bg-gray-100 text-gray-900 text-xs py-1 px-2 rounded">Copied!</p>
         </div>
     {/if}
